c681fddc0a6281f236bc4e74da02202df78ff37c
Caio Cesar <cesar.oliveira.c@hotmail.com>
2021-01-28 06:30:23 -0300

    Example of set - Teacher solution

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,29 +1,47 @@
 package application;
 
+import java.text.ParseException;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 
-import entites.Course;
-
 public class Program {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws ParseException {
+
 		Scanner sc = new Scanner(System.in);
-		Set<Course> set = new HashSet<>();
+
+		Set<Integer> a = new HashSet<>();
+		Set<Integer> b = new HashSet<>();
+		Set<Integer> c = new HashSet<>();
 		
-		for (int i = 0; i < 3; i++) {
-			String letter = "ABC";
-			System.out.print("How many students for course " + letter.charAt(i) + "? ");
-			int n = sc.nextInt();
-			for (int j = 0; j < n; j++) {
-				int cod = sc.nextInt();
-				set.add(new Course(cod));
-			}
+		System.out.print("How many students for course A? ");
+		int n = sc.nextInt();
+		for (int i=0; i<n; i++) {
+			int number = sc.nextInt();
+			a.add(number);
 		}
-		System.out.println("Total students: " + set.size());
+
+		System.out.print("How many students for course B? ");
+		n = sc.nextInt();
+		for (int i=0; i<n; i++) {
+			int number = sc.nextInt();
+			b.add(number);
+		}
+
+		System.out.print("How many students for course C? ");
+		n = sc.nextInt();
+		for (int i=0; i<n; i++) {
+			int number = sc.nextInt();
+			c.add(number);
+		}
+		
+		Set<Integer> total = new HashSet<>(a);
+		total.addAll(b);
+		total.addAll(c);
+
+		System.out.println("Total students: " + total.size());
+
 		sc.close();
 	}
-
 }
